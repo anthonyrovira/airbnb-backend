38515c06e43f77402c0531cc55107e3d8c1c87cc
hysteria9 <anthonyrov@gmail.com>
2021-02-26 21:02:47 +0100

    fix: trying to fix axios Network error

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ cloudinary.config({
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(("Access-Control-Allow-Headers": "*"));
+  res.header(("Access-Control-Allow-Headers", "*"));
   next();
 });
 
